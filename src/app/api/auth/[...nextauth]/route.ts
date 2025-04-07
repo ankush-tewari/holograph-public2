e5c1f5273514180738fcabfa543bbc3762a8181a
ankush-tewari <ankush@holographcompany.com>
2025-04-07 17:01:20 -0400

    continuing debugging why we can't find the db in prod

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,12 @@
 // src/app/api/auth/[...nextauth]/route.ts
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic"; // for debugging
+// ✅ ADD this line:
+export const runtime = "nodejs"; //again for debugging
 import NextAuth from 'next-auth';
 import { debugLog } from '@/utils/debug';
 import { getAuthOptions } from '@/lib/auth';
 
-if (!getAuthOptions) {
-  console.error("❌ authOptions is undefined! Check if `src/lib/auth.ts` exists.");
-}
 
 const handler = async (...args: any[]) => {
   debugLog("📦 Loading auth handler dynamically");
